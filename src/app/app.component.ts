8a0235d52a61cd48166200aa480db9f16d2dd049
Santosh Behera01 <Santosh.Behera01@godigit.com>
2023-01-22 23:10:36 +0530

    trackBy with ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,68 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'trackBy';
+
+
+  employee=[
+  {
+    name:"Manoj",
+    age: 26,
+    email:"manoj@gmail.com",
+    salary: 25000
+  },
+  {
+    name:"Sohan",
+    age: 28,
+    email:"sohan@gmail.com",
+    salary: 28000
+  },
+  {
+    name:"Shruti",
+    age: 30,
+    email:"sruti@gmail.com",
+    salary: 35000
+  }
+]
+
+// Load more employee
+loadMoreEmployee():void{
+ this.employee=[
+    {
+      name:"Manoj",
+      age: 26,
+      email:"manoj@gmail.com",
+      salary: 25000
+    },
+    {
+      name:"Sohan",
+      age: 28,
+      email:"sohan@gmail.com",
+      salary: 28000
+    },
+    {
+      name:"Shruti",
+      age: 30,
+      email:"sruti@gmail.com",
+      salary: 35000
+    },
+    {
+      name:"Suresh",
+      age: 35,
+      email:"suresh@gmail.com",
+      salary: 45000
+    },
+    {
+      name:"Bikash",
+      age: 36,
+      email:"bikash@gmail.com",
+      salary: 55000
+    },
+  ]
+}
+
+getEmployeeUsingTrackBy(index:number,employee:any):string{
+  return employee.name;
+}
+
+
 }
